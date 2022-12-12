1229885e54aeceaf7b029138900242f92030bf6f
RenatDavletshin <immortal1307@gmail.com>
2022-12-12 19:58:17 +0500

    ForUnderstand

---
+++ For.py
@@ -0,0 +1,27 @@
+# for
+
+'''
+for i in 1, 2, 3, 4, 5:
+    print(i**2)
+    '''
+
+
+'''
+list = [1, 2, 3, 4, 5]
+for i in list:
+    print(i**2)
+    '''
+
+'''
+r = range(10)
+for i in r:     # for i in range(10):
+    print(i)
+    '''
+
+'''
+for i in range(1, 10, 2):
+    print(i)
+    '''
+
+for i in 'qwerty':
+    print(i)
