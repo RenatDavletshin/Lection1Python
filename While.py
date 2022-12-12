ef83472b757c98d92f860213a0b0c5166dca48e0
RenatDavletshin <immortal1307@gmail.com>
2022-12-12 19:25:41 +0500

    WhileUnderstand

---
+++ While.py
@@ -0,0 +1,19 @@
+'''
+original = 23
+inverted = 0
+while original != 0:
+    inverted = inverted * 10 + (original % 10)
+    original //= 10
+print(inverted)
+'''
+
+# While-Else
+original = 23
+inverted = 0
+while original != 0:
+    inverted = inverted * 10 + (original % 10)
+    original //= 10
+else:
+    print('Пожалуй')
+    print('Хватит')
+print(inverted)
