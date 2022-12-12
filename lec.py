1571001de9cd4538f08aa5d62dd676c2a314a5e1
RenatDavletshin <immortal1307@gmail.com>
2022-12-12 17:20:21 +0500

    Understand

---
+++ lec.py
@@ -0,0 +1,36 @@
+'''
+a = 123
+b = 1.23
+s = 'hello world'
+
+# print(a, ' - ',b, ' - ', s)
+# print('{} - {} - {}'.format(a, b, s))
+print(f'{a} - {b} - {s}')
+f = True
+print(f)
+'''
+
+'''
+list = ['1', '2', '3']
+col = ['hello', 1,2,4.5, True]
+print(list)
+print(col)
+'''
+'''
+print('Введите a')
+a = int(input())
+print('Введите b')
+b = int(input())
+print(a, ' + ' , b, ' = ', a + b)
+'''
+
+print('Введите a')
+a = float(input())
+print('Введите b')
+b = float(input())
+print(a, ' + ' , b, ' = ', a + b)
+
+'''
+print('{} {}'.format(a, b))
+print(f'{a} {b}')
+'''
