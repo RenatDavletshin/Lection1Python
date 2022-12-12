746bab24ab82b875e0d081c9df303ca7f44b8dec
RenatDavletshin <immortal1307@gmail.com>
2022-12-12 17:42:22 +0500

    LogicEnd

---
+++ logic.py
@@ -0,0 +1,31 @@
+'''
+Логические операции
+>, >=, <, <=, ==, !=
+not, and, or - не путать с &, |, ^
+is, is not, in, not in
+gen
+'''
+
+'''
+a = 1 < 4 and 5 > 2
+print(a)
+'''
+
+'''
+func = 1
+T = 4
+x = 2
+print(func<T>(x))
+'''
+
+'''
+f = 1 > 2 or 4 < 6
+
+print(f)
+'''
+
+f = [1, 2, 3, 4]
+# print(f)
+# print(2 in f)
+is_odd = not f[0] % 2
+print(is_odd)
